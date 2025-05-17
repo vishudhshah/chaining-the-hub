ff4ff2d8da210dc55a9bde75c5b08428f210d4c6
Vishudh Shah <vishudh@hotmail.com>
2025-05-17 14:39:19 -0700

    step 1

---
+++ map.js
@@ -0,0 +1,18 @@
+// Import Mapbox as an ESM module
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+// Check that Mapbox GL JS is loaded
+console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoidjhzaGFoIiwiYSI6ImNtYXNkY2NrcjBtYmoybHB5NzZ5a3FjeW0ifQ.1VgPGJja7f7qGXMEhA6VLQ';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Map style; can be customized using Mapbox Studio
+    center: [-71.09415, 42.36027], // [longitude, latitude]
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum allowed zoom
+    maxZoom: 18, // Maximum allowed zoom
+});
