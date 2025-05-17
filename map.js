e9fadc0c43f39f2e0a3a9365ffbf5b0f4156c079
Vishudh Shah <vishudh@hotmail.com>
2025-05-17 14:54:58 -0700

    Add bike lane sources and layers for Boston and Cambridge to the map

--- map.js
+++ map.js
@@ -16,3 +16,37 @@ const map = new mapboxgl.Map({
     minZoom: 5, // Minimum allowed zoom
     maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+    });
+
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400',
+            'line-width': 5,
+            'line-opacity': 0.6,
+        },
+    });
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+    });
+
+    map.addLayer({
+        id: 'cambridge-bike-lanes',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': '#32D400',
+            'line-width': 5,
+            'line-opacity': 0.6,
+        },
+    });
+});
